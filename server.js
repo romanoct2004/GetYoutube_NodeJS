5acdbf7f93b7c5407dd7cbcbed24afc244cbe9bd
romanoct2004 <romanoct2004@gmail.com>
2021-06-04 17:06:39 +0700

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const ytdl = require('ytdl-core');
-
-async function getInforVideo(){
-    let info = await ytdl.getInfo("oQXgDshfQ0s");
-    //let format = ytdl.chooseFormat(info.formats, { quality: '134' });
-    console.log(info);
-}
-
-getInforVideo();
